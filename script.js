73d4e01a91e11d1e5ed684008dae1cc194e72475
rahulreena <rahulreena00@gmail.com>
2023-04-04 23:21:58 +0530

    a new change has been made

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.nav-main');
+let input = document.querySelector('#menu-icon-2');
+let inputbox = document.querySelector('.input-box');
 
 menu.onclick = ()=>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+}
+
+input.onclick = ()=>{
+    input.classList.toggle('bx-x');
+    inputbox.classList.toggle('open1');
 }
